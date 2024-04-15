64e3505f87cfe088359ac899ad7e88a62e4f2412
micky0008 <micky0008@hotmail.fr>
2024-04-15 14:43:50 +0200

    wip

--- src/app/boite-parent/boite-parent.component.ts
+++ src/app/boite-parent/boite-parent.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, WritableSignal, effect } from '@angular/core';
-import { MessageService } from 'primeng/api';
+import { MessageService, TreeNode } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
 import { InplaceModule } from 'primeng/inplace';
@@ -16,29 +16,30 @@ import { Lieu } from '../entites/Lieu';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { FormsModule } from '@angular/forms';
 import { BoiteComponent } from '../boite/boite.component';
+import { OrganizationChartModule } from 'primeng/organizationchart';
 
 
 @Component({
   selector: 'app-boite-parent',
   standalone: true,
-  imports: [BoiteComponent,ButtonModule, InplaceModule, OverlayPanelModule, TableModule, ToastModule, TooltipModule, DropdownModule, InputTextareaModule, FormsModule],
+  imports: [BoiteComponent, ButtonModule, InplaceModule, OverlayPanelModule, TableModule, ToastModule, TooltipModule, DropdownModule, InputTextareaModule, FormsModule, OrganizationChartModule],
   templateUrl: './boite-parent.component.html',
   styleUrl: './boite-parent.component.css'
 })
 export class BoiteParentComponent implements OnInit {
-  
-  boites: Boite[] = [];
+
+  boites: TreeNode<Boite>[] = [];
   lieux: Lieu[] = [];
   pieces: Piece[] = [];
-  selectedPiece:WritableSignal<Piece>;
+  selectedPiece: WritableSignal<Piece>;
 
   constructor(private lieuService: LieuService,
     private boiteService: BoiteService,
     private pieceService: PieceService) {
-      this.selectedPiece = this.pieceService.piece;
+    this.selectedPiece = this.pieceService.piece;
   }
 
-  ngOnInit(): void { 
+  ngOnInit(): void {
     this.lieuService.getAll().then(newLieux => this.lieux = newLieux);
   }
 
@@ -49,6 +50,69 @@ export class BoiteParentComponent implements OnInit {
 
   changePiece(newPiece: Piece) {
     this.pieceService.piece.set(newPiece);
-    this.boiteService.getAllByPiece(newPiece).then(allBoites => this.boites = allBoites);
+    this.boiteService.getAllByPiece(newPiece).then((allBoites: Boite[]) => {
+      let cloneNewpiece = { ...newPiece };
+      cloneNewpiece.uuid = '00000000-0000-0000-0000-000000000001';
+      let tn: TreeNode = {
+        label: newPiece.nom,
+        data: cloneNewpiece,
+        expanded: true,
+        children: []
+      }
+      for (let boite of allBoites) {
+        if (tn.children == null) {
+          tn.children = [];
+        }
+        tn.children.push(this.convertBoiteToTreeNode(boite));
+      }
+      this.boites.push(tn);
+    });
   }
+
+  private convertBoiteToTreeNode(boite: Boite): TreeNode<Boite> {
+    let tn: TreeNode = {
+      label: boite.nom,
+      data: boite,
+      children: [],
+      expanded: true
+    }
+    if (boite.boites != null) {
+      for (let b of boite.boites) {
+        if (tn.children == null) {
+          tn.children = [];
+        }
+        tn.children.push(this.convertBoiteToTreeNode(b));
+      }
+    }
+    return tn;
+  }
+
+
+  addBoite(parentBoite: Boite) {
+    if (this.boites[0] == undefined) {
+      return;
+    }
+    let firstTreeNode = this.boites[0];
+    let treeNode = this.findUuidBoiteInTreenode(firstTreeNode, parentBoite.uuid as string);
+    console.log(treeNode);
+  }
+
+
+  private findUuidBoiteInTreenode(treenodeParent: TreeNode, uuidBoite: string): TreeNode|null { //treenodeParent = grenier //uuidBoite = petite boite
+    if (treenodeParent.children == null || treenodeParent.children.length == 0) {
+      return null;
+    }
+    let res: TreeNode | null = null;
+    for (let treenode of treenodeParent.children) { //grande boite et boite a outils
+      if (treenode.data.uuid == uuidBoite) {
+        return treenode;        
+      }
+      res = this.findUuidBoiteInTreenode(treenode, uuidBoite); //grande boite
+      if (res != null) {
+        return res;
+      }
+    }
+    return null;
+  }
+
 }
